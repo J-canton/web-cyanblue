3c2d827c2fcdbaf7f8e354bceb2aba7d450ca346
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2021-02-01 11:38:36 +0100

    add styles to form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 class ContactForm extends React.Component{
   constructor(props){
@@ -9,7 +10,13 @@ class ContactForm extends React.Component{
       subject: '',
       msg: ''
     };
+    this.templateParams = {
+      from_name: `${this.state.name} ( ${this.state.email} )`,
+      feedback: `(${this.state.subject}) -> ${this.state.msg}`
+    };
   }
+
+
 
   isNotEmpty =(item)=>{
     if(item !== '' && item !== null && item !==undefined){
@@ -32,8 +39,21 @@ class ContactForm extends React.Component{
     event.preventDefault();
     console.log(this.isFormComplete());
     if(this.isFormComplete()){
-      console.log('state->', this.state);
+      this.sendEmail();
     }
+  }
+
+  sendEmail = ()=>{
+    emailjs
+      .send("service_e1pgkut", "template_hymgfko", this.templateParams, 'user_tRjyqZ4k8zS0Cy5XozirE')
+      .then(
+        function(response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function(err) {
+          console.log('ERROR->', err)
+        }
+      );
   }
 
 
@@ -44,35 +64,48 @@ class ContactForm extends React.Component{
         <h2>{this.props.title}</h2>
         <div className="form-content">
           <div className="form-textarea-content">
-            <textarea 
-              value={this.state.msg} 
-              onChange={(msg)=>{this.setState({msg: msg.target.value})}} 
-              placeholder="Write Message"
-            /> 
+            <div className="form-input">
+              <textarea 
+                value={this.state.msg} 
+                onChange={(msg)=>{this.setState({msg: msg.target.value})}} 
+                placeholder="Write Message"
+              /> 
+            </div>
           </div>
           <div className="form-input-content">
-            <input 
-              className="inputName" 
-              value={this.state.name} 
-              onChange={(name)=>{this.setState({name: name.target.value})}} 
-              placeholder="Your Name"
-            />  
-            <input 
-              className="inputEmail" 
-              value={this.state.email}
-              onChange={(email)=>{this.setState({email:email.target.value})}}
-              placeholder="Your Email"/>
+            <div className="form-double-input">
+              <div className="form-input">
+                <input 
+                  className="inputName" 
+                  value={this.state.name} 
+                  onChange={(name)=>{this.setState({name: name.target.value})}} 
+                  placeholder="Your Name"
+                />
+              </div>
+              <div className="form-input"> 
+                <input 
+                  className="inputEmail" 
+                  value={this.state.email}
+                  onChange={(email)=>{this.setState({email:email.target.value})}}
+                  placeholder="Your Email"
+                />
+              </div>
+            </div>
           </div>
-          <input
-          className="inputSubject"
-          value={this.state.subject}
-          onChange={(subject)=>{this.setState({subject: subject.target.value})}} 
-          placeholder="Subject"/>
-        </div>
-        <div
-          onClick={this.sendForm}
-        >
-          <span>SEND</span>
+          <div className="form-input">
+            <input
+              className="inputSubject full-input"
+              value={this.state.subject}
+              onChange={(subject)=>{this.setState({subject: subject.target.value})}} 
+              placeholder="Subject"
+            />
+          </div>
+          <div
+            className="btn"
+            onClick={this.sendForm}
+          >
+            <span>SEND</span>
+          </div>
         </div>
       </div>
     )
